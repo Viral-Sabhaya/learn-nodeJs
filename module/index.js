859e2dce55450f038b2fa99a1d67bed0ae52f6e5
Viral-Sabhaya <sabhayaviral48@gmail.com>
2023-07-12 21:54:49 +0530

    first commit

---
+++ module/index.js
@@ -0,0 +1,7 @@
+const path = require("path");
+const pathDir = path.dirname("foo/gaa/jaa/viral.html");
+const pathExt = path.extname("foo/gaa/jaa/viral.html");
+const pathBase = path.basename("foo/gaa/jaa/viral.html");
+console.log(pathDir);
+console.log(pathExt);
+console.log(pathBase);
