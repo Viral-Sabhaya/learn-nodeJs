af2a0f499d5f604a3aaf72ef176324544f59fe54
Viral-Sabhaya <sabhayaviral48@gmail.com>
2023-07-12 23:31:59 +0530

    add server and nodemon

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const port = 3000;
+
+// create server
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.end("hii this is my first nodejs server");
+  } else if (req.url == "/data") {
+    res.end("hii this is my first data page server");
+  } else {
+    res.end("page not found!!");
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server is running port no ${port}`);
+});
