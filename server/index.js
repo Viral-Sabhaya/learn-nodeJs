e25e3db24eafa7c4e10d270af1f9ebfd92797080
Viral-Sabhaya <sabhayaviral48@gmail.com>
2023-07-13 20:39:33 +0530

    add data of api

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
+const userData = require("./api.json");
+const fs = require("fs");
 const http = require("http");
 const port = 3000;
 
 // create server
 const server = http.createServer((req, res) => {
+  const jsonData = fs.readFileSync("api.json", "utf-8");
+  const objData = JSON.parse(jsonData);
+
   if (req.url == "/") {
     res.end("hii this is my first nodejs server");
   } else if (req.url == "/data") {
     res.end("hii this is my first data page server");
   } else if (req.url == "/about") {
-    res.end("hii this is my about page server");
+    res.end(objData[0].name);
   } else {
-    res.end("page not found!!");
+    res.end("404 : page not found!!");
   }
 });
 
