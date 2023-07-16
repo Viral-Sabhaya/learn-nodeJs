833b1e4dec21ae7b5ee7b9a1ebc1cefb809b8cc8
Viral-Sabhaya <sabhayaviral48@gmail.com>
2023-07-16 15:31:39 +0530

    add ejs and twilandcss

--- express/index.js
+++ express/index.js
@@ -2,22 +2,30 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
-const pathDir = path.join(__dirname, "public");
 
-app.use(express.static(pathDir));
+app.set("view engine", "ejs");
+
+// const pathDir = path.join(__dirname, "public");
+// app.use(express.static(pathDir));
 
 app.get("/", (req, res) => {
-  res.sendFile(`${pathDir}/index.html`);
+  res.render("index", {
+    title: "index file",
+  });
+  // res.sendFile(`${pathDir}/index.html`);
 });
 
 app.get("/home", (req, res) => {
-  res.sendFile(`${pathDir}/home.html`);
+  res.render("home", {
+    title: "home file",
+  });
+  // res.sendFile(`${pathDir}/home.html`);
 });
 
 app.get("/download", (req, res) => {
-  res.download(`${pathDir}/index.html`);
+  // res.download(`${pathDir}/index.html`);
 });
 
 app.listen(port, () => {
-  console.log("server is running");
+  console.log(`server is running ${port}`);
 });
